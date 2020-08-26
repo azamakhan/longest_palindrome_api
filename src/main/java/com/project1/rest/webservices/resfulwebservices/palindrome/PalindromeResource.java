bf3d40b9e37a642408e297aaa5171397024f1a30
Azam Khan <azamcr7@gmail.com>
2020-08-26 13:55:15 -0400

    2nd commit

--- src/main/java/com/project1/rest/webservices/resfulwebservices/palindrome/PalindromeResource.java
+++ src/main/java/com/project1/rest/webservices/resfulwebservices/palindrome/PalindromeResource.java
@@ -1,9 +1,6 @@
 package com.project1.rest.webservices.resfulwebservices.palindrome;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PalindromeResource {
 	
-//	@Autowired
-//	private PalindromeDaoService servicePal;
-//	
-//	@Autowired
-//	private PalindromeRepository repositoryPal;
-
 	@Autowired
 	private PalindromeService servicePal;
 	
--- src/main/java/com/project1/rest/webservices/resfulwebservices/palindrome/PalindromeService.java
+++ src/main/java/com/project1/rest/webservices/resfulwebservices/palindrome/PalindromeService.java
@@ -1,8 +1,5 @@
 package com.project1.rest.webservices.resfulwebservices.palindrome;
 
-import java.util.Optional;
-
-import org.springframework.web.bind.annotation.PathVariable;
 
 public interface PalindromeService {
 
